c546e47edcf86a9b737f94f907a437f4dd9cefff
shipper1953 <tagregg22@gmail.com>
2025-04-28 18:54:01 -0500

    fix: add vite-plugin-commonjs to fix react-redux build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import commonjs from 'vite-plugin-commonjs'; // 🆕
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    commonjs(), // 🆕 ADD THIS
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
